789ef641d2dd4e4451cb7ab59ca5f482b184931f
yogesh6499 <98866907+yogesh6499@users.noreply.github.com>
2023-01-15 12:24:48 +0530

    git commit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'app/product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  albumInfo: any;
 
-  constructor() { }
+  constructor(private _productService:ProductService) { }
 
   ngOnInit(): void {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+    
   }
 
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private _http: Http) { }
+  private __albumUrl:string='../assets/album.json';
+  public getAlbum(id:number):Observable<any>{
+    return this._http.get(this.__albumUrl).map((response => {
+       response.json();
+    }));
+  }
+}
