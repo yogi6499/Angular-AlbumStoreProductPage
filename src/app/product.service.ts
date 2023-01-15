013f5f21d3ccf6a34db1e1fdfb0b50091282c894
yogesh6499 <98866907+yogesh6499@users.noreply.github.com>
2023-01-15 16:33:46 +0530

    git commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,8 +11,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
   getAlbum(id: number):Observable<Album>{
-    return this._http.get(this._albumUrl).map<Album>((response) => 
-      response.json());
+    return this._http.get(this._albumUrl).map((response) => 
+    <Album>response.json());
     
   }
 }
