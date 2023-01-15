6a977f14606e3b38e4af7efecfa39db8a60e5a1a
yogesh6499 <98866907+yogesh6499@users.noreply.github.com>
2023-01-15 13:31:30 +0530

    git commit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,7 +7,7 @@ import { ProductService } from 'app/product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  public albumInfo;
+  albumInfo;
 
   constructor(private _productService:ProductService) { }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,10 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
+  private __albumUrl= '../assets/album.json';
 
   constructor(private _http: Http) { }
-  public getAlbum(id:number){
-    this.__albumUrl= this._http.get(this.__albumUrl).map((response => {
+  public getAlbum(id: number){
+    return this._http.get(this.__albumUrl).map((response => {
       this.__albumUrl=response.json();
     }));
   }
